f134ee11e72c5440035f0fe920851b106fe92081
JordanPC <Galacticvoice@gmail.com>
2021-05-15 09:33:25 -0400

    Quick commit before class starts

---
+++ client/src/pages/dashboard.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Jumbotron  from "../components/Jumbotron";
+import {Link} from "react-router-dom";
+import {Col, Row, Container} from "../components/Grid";
+
+function Dashboard() {
+    return(
+
+        
+
+        <Container fluid>
+            <Row>
+                <Col>
+                    <Jumbotron>
+                        <h1> DASHBOARD </h1>
+                         
+                    </Jumbotron>
+                        
+                </Col>
+            </Row>
+            <Row>
+                <h1> BUTTONS IN PROGRESS GO HERE</h1>
+            </Row>
+        </Container>
+    );
+}
+
+export default Dashboard;
---
+++ client/src/pages/selection.js
@@ -0,0 +1,4 @@
+import React from "react";
+import Jumbotron  from "../components/Jumbotron";
+import {Link} from "react-router-dom";
+import {Col, Row, Container} from "../components/Grid";
