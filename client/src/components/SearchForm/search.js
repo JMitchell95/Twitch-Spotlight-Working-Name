a0572c50996c9c3fbb6121cbfddcd99afab849e2
unknown <nukasine@gmail.com>
2021-05-22 15:34:17 -0600

    put backend into seperate folder and added file structure needed for authentication. Moved all backend code to a seperate backend folder

--- client/src/components/SearchForm/search.js
+++ client/src/components/SearchForm/search.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 function Search() {
+  const [searchState, setSearchState] = useState({
+
+  });
+
   return(
 <select class="form-select" aria-label="Default select example">
   <option selected>Viewer count</option>
-  <option value="1">0-1000</option>
-  <option value="2">1000-3000</option>
-  <option value="3">3000-5000</option>
+  <option value="0-1000">0-1000</option>
+  <option value="1000-3000">1000-3000</option>
+  <option value="3000-5000">3000-5000</option>
+  <option value="5001-10000">5001-10000</option>
+  <option value="10001-15000">10001-15000</option>
+  <option value="15001-50000">15001-50000</option>
 </select>
     
   );
+
 }
 
 
