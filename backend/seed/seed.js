469955759619c03f809f8eed2578a832e3db648e
JordanPC <Galacticvoice@gmail.com>
2021-05-25 19:42:43 -0400

    need to hook login

---
+++ backend/seed/seed.js
@@ -0,0 +1,27 @@
+  
+const mongoose = require("mongoose");
+const db = require("../models");
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/sptligt");
+
+const dummyUser = [
+    {
+        username: "admin",
+        email: "admin@admin.com",
+        password: "password",
+        
+    },
+
+];
+
+
+db.User.remove({})
+  .then(() => db.User.collection.insertMany(dummyUser))
+  .then(data => {
+    console.log(data.result.n + "Dummy Users Created");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
